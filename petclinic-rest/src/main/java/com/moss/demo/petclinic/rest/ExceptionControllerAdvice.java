854093609be1daaed7c52f0deee9fc6a3a44cd94
Moss GU <gufeifei@outlook.com>
2018-03-04 18:28:55 +1300

    Exception handler

---
+++ petclinic-rest/src/main/java/com/moss/demo/petclinic/rest/ExceptionControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.moss.demo.petclinic.rest;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorInfo errorInfo = new ErrorInfo(e);
+        String respJSONstring = "{}";
+
+        try {
+            respJSONstring = mapper.writeValueAsString(errorInfo);
+        } catch (JsonProcessingException e1) {
+            e1.printStackTrace();
+        }
+        return ResponseEntity.badRequest().body(respJSONstring);
+    }
+
+
+    private class ErrorInfo {
+
+        public final String className;
+        public final String exMessage;
+
+        public ErrorInfo(Exception ex) {
+            this.className = ex.getClass().getName();
+            this.exMessage = ex.getLocalizedMessage();
+        }
+    }
+}
+
