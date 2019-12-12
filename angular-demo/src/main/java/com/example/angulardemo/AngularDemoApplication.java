08d6f42f13a56b7fa5a7d00709408d6a0fae2b2d
Moss GU <gufeifei@outlook.com>
2019-12-12 18:03:17 +1300

    add angular demo project

---
+++ angular-demo/src/main/java/com/example/angulardemo/AngularDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.angulardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularDemoApplication.class, args);
+	}
+
+}
---
+++ angular-demo/src/test/java/com/example/angulardemo/AngularDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angulardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
